8d43ec8eec92a9fe48b6cfd0da9c7bee3d577c89
antoinekc <kimkimkimkim0719@gmail.com>
2024-10-02 16:33:02 +0200

    deploy

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ module.exports = router;
 const API_KEY = process.env.API_KEY
 
 router.get('/movies', (req, res) => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=7d49be1df05395aaa24891e22721dc77`)
   .then(response => response.json())
   .then(data => {
     res.json({ movies: data.results})
